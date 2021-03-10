4ec45cf902dc01359a2106252cc289d03767c61e
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2021-03-10 14:29:44 -0600

    ToC block: use static markup and only support core Heading and Page Break blocks.

--- packages/block-library/src/table-of-contents/index.js
+++ packages/block-library/src/table-of-contents/index.js
@@ -9,6 +9,7 @@ import { __ } from '@wordpress/i18n';
 import metadata from './block.json';
 import edit from './edit';
 import icon from './icon';
+import save from './save';
 
 const { name } = metadata;
 
@@ -22,4 +23,5 @@ export const settings = {
 	icon,
 	keywords: [ __( 'document outline' ), __( 'summary' ) ],
 	edit,
+	save,
 };
