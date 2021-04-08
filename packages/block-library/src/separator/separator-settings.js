2f9669222827eeed7cc634d28a2468d6d7c1c4d6
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-04-08 15:53:40 +1000

    Add top/bottom margins to separator block
    This leverages the modified BoxControl component from `update/boxcontrol-with-configurable-sides` to allow only top and bottom margins to be set.

--- packages/block-library/src/separator/separator-settings.js
+++ packages/block-library/src/separator/separator-settings.js
@@ -3,20 +3,74 @@
  */
 import { __ } from '@wordpress/i18n';
 import { InspectorControls, PanelColorSettings } from '@wordpress/block-editor';
+import {
+	PanelBody,
+	__experimentalBoxControl as BoxControl,
+} from '@wordpress/components';
 
-const SeparatorSettings = ( { color, setColor } ) => (
-	<InspectorControls>
-		<PanelColorSettings
-			title={ __( 'Color' ) }
-			colorSettings={ [
-				{
-					value: color.color,
-					onChange: setColor,
-					label: __( 'Color' ),
+/**
+ * Internal dependencies
+ */
+import { CSS_UNITS } from './shared';
+
+const SeparatorSettings = ( {
+	color,
+	setColor,
+	attributes: { style },
+	setAttributes,
+} ) => {
+	const updateMargins = ( { top, bottom } ) => {
+		setAttributes( {
+			style: {
+				...style,
+				spacing: {
+					...style?.spacing,
+					margin: { top, bottom },
 				},
-			] }
-		></PanelColorSettings>
-	</InspectorControls>
-);
+			},
+		} );
+	};
+
+	const onChangeShowVisualizer = ( { top, bottom } ) => {
+		setAttributes( {
+			style: {
+				...style,
+				visualizers: {
+					margin: { top, bottom },
+				},
+			},
+		} );
+	};
+
+	return (
+		<InspectorControls>
+			<PanelColorSettings
+				title={ __( 'Color settings' ) }
+				colorSettings={ [
+					{
+						value: color.color,
+						onChange: setColor,
+						label: __( 'Color' ),
+					},
+				] }
+			/>
+			<PanelBody title={ __( 'Separator settings' ) }>
+				<BoxControl
+					label={ __( 'Margin' ) }
+					onChange={ updateMargins }
+					sides={ {
+						top: true,
+						right: false,
+						bottom: true,
+						left: false,
+					} }
+					units={ CSS_UNITS }
+					values={ style?.spacing?.margin || {} }
+					onChangeShowVisualizer={ onChangeShowVisualizer }
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
 
 export default SeparatorSettings;
